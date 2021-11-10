5536741b27986c795a7e0b4650b8a327cf58ed0e
gvander <titavdb@icloud.com>
2021-11-10 14:34:42 +0100

    lecture des données

--- ProjetSimulation.c
+++ ProjetSimulation.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// structure tableau, jour, températures moyenne de surface, précipitation totale 
+struct donneesGlacier {
+    char jour;
+    double température;
+    double prcipitation;  
+};
 
 int main(int argc, char * argv[]) {
-    printf("Hello, world!\n");
-    return 0;
+    // Tableau avec les moyennes des températures par jours et les précipitations au sommet du glacier
+    // 366 lignes (dates) 3 collones jours, températures, précpitations
+    double * 
 }
